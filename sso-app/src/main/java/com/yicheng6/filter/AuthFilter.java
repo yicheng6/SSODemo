1bbf9772edcd122590ff7db3f5c99f4f9e7a0f9e
yicheng6 <1415252183@qq.com>
2015-12-14 07:49:46 +0800

    实现应用系统拦截url重定向认证系统

---
+++ sso-app/src/main/java/com/yicheng6/filter/AuthFilter.java
@@ -0,0 +1,66 @@
+package com.yicheng6.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthFilter implements Filter {
+	
+	private String authService;
+	
+	private String loginUrl;
+	
+	private String cookieName;
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+		
+		//跳转登陆地址参数
+		String param = request.getQueryString();
+		String URL = loginUrl + "?gotoUrl=" + (param != null ? request.getRequestURL().append("?").append(param).toString() : request.getRequestURI());
+		
+		//获取指定cookie进行分发
+		Cookie ticket = null;
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookieName.equals(cookie.getName())) {
+					ticket = cookie;
+					break;
+				}	
+			}
+		}
+		if (ticket != null) 
+			authCookie();
+		else
+			response.sendRedirect(URL);
+	}
+	
+	public void authCookie() {
+		
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		authService = filterConfig.getInitParameter("authService");
+		loginUrl = filterConfig.getInitParameter("loginUrl");
+		cookieName = filterConfig.getInitParameter("cookieName");
+	}
+
+}
