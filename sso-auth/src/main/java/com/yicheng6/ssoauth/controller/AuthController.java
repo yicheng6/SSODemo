a2d597bdd87853385699733a78c262a8b66ad033
yicheng.liu <yicheng.liu@ximalaya.com>
2015-12-14 00:04:14 +0800

    环境搭建

---
+++ sso-auth/src/main/java/com/yicheng6/ssoauth/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.yicheng6.ssoauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthController {
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+}
